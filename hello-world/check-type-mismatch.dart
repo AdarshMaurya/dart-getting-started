1dc95675f3d7e918083829db7d04fd2427891324
Adarsh Maurya <adarshmaurya@softhinkers.com>
2018-12-08 05:57:48 +0530

    - checked mode

---
+++ hello-world/check-type-mismatch.dart
@@ -0,0 +1,18 @@
+void main(){
+  int n = "hello";
+  print(n);
+}
+//console outputs:
+/*
+
+Try changing the type of the left hand side, or casting the right hand side to 'dart.core::int'.
+  int n = "hello";
+          ^
+Adarsh:hello-world adarshmaurya$ dart -c  check-type-mismatch.dart
+check-type-mismatch.dart:2:11: Error: A value of type 'dart.core::String' can't be assigned to a variable of type 'dart.core::int'.
+Try changing the type of the left hand side, or casting the right hand side to 'dart.core::int'.
+  int n = "hello";
+          ^
+Adarsh:hello-world adarshmaurya$
+
+ */
