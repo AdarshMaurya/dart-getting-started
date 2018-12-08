d9761faf3cc8b6b2241b2a1f6edbf3688f104ad4
Adarsh Maurya <adarshmaurya@softhinkers.com>
2018-12-08 06:50:31 +0530

    - Strings - single and multi-lines

--- hello-world/common-syntax.dart
+++ hello-world/common-syntax.dart
@@ -55,4 +55,22 @@ void main() {
   //Parsing
   print(num.parse('12'));
   print(num.parse('10.91'));
+
+
+  String str1 = 'this is a single line string';
+  String str2 = "this is a single line string";
+  String str3 = '''this 
+  is a 
+  multiline line 
+  string
+ ''';
+  String str4 = """this 
+  is a 
+  multiline line 
+  string""";
+
+  print(str1);
+  print(str2);
+  print(str3);
+  print(str4);
 }
